e8ba1d9412535415679f993ce076138e78733231
Fredrik Fornwall <fredrik@fornwall.net>
2019-09-22 22:37:58 +0200

    Update to JUnit 5

--- src/test/java/net/fornwall/jelf/BasicTest.java
+++ src/test/java/net/fornwall/jelf/BasicTest.java
@@ -1,7 +1,7 @@
 package net.fornwall.jelf;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,9 +20,9 @@ public class BasicTest {
 			String expected = expectedSectionNames[i];
 			String actual = file.getSection(i).getName();
 			if (expected == null) {
-				Assert.assertNull(actual);
+				Assertions.assertNull(actual);
 			} else {
-				Assert.assertEquals(expected, actual);
+				Assertions.assertEquals(expected, actual);
 			}
 		}
 	}
@@ -30,20 +30,20 @@ public class BasicTest {
 	@Test
 	public void testAndroidArmBinTset() throws ElfException, FileNotFoundException, IOException {
 		ElfFile file = parseFile("android_arm_tset");
-		Assert.assertEquals(ElfFile.CLASS_32, file.objectSize);
-		Assert.assertEquals(ElfFile.DATA_LSB, file.encoding);
-		Assert.assertEquals(ElfFile.FT_EXEC, file.file_type);
-		Assert.assertEquals(ElfFile.ARCH_ARM, file.arch);
-		Assert.assertEquals(32, file.ph_entry_size);
-		Assert.assertEquals(7, file.num_ph);
-		Assert.assertEquals(52, file.ph_offset);
-		Assert.assertEquals(40, file.sh_entry_size);
-		Assert.assertEquals(25, file.num_sh);
-		Assert.assertEquals(15856, file.sh_offset);
+		Assertions.assertEquals(ElfFile.CLASS_32, file.objectSize);
+		Assertions.assertEquals(ElfFile.DATA_LSB, file.encoding);
+		Assertions.assertEquals(ElfFile.FT_EXEC, file.file_type);
+		Assertions.assertEquals(ElfFile.ARCH_ARM, file.arch);
+		Assertions.assertEquals(32, file.ph_entry_size);
+		Assertions.assertEquals(7, file.num_ph);
+		Assertions.assertEquals(52, file.ph_offset);
+		Assertions.assertEquals(40, file.sh_entry_size);
+		Assertions.assertEquals(25, file.num_sh);
+		Assertions.assertEquals(15856, file.sh_offset);
 		assertSectionNames(file, null, ".interp", ".dynsym", ".dynstr", ".hash", ".rel.dyn", ".rel.plt", ".plt", ".text");
 
 		ElfSection dynamic = file.getDynamicLinkSection();
-		Assert.assertNotNull(dynamic);
+		Assertions.assertNotNull(dynamic);
 		// typedef struct {
 		// Elf32_Sword d_tag;
 		// union {
@@ -51,13 +51,13 @@ public class BasicTest {
 		// Elf32_Addr d_ptr;
 		// } d_un;
 		// } Elf32_Dyn;
-		Assert.assertEquals(8, dynamic.entry_size);
-		Assert.assertEquals(248, dynamic.size);
+		Assertions.assertEquals(8, dynamic.entry_size);
+		Assertions.assertEquals(248, dynamic.size);
 
 		ElfDynamicStructure ds = dynamic.getDynamicSection();
-		Assert.assertEquals(Arrays.asList("libncursesw.so.6", "libc.so", "libdl.so"), ds.getNeededLibraries());
+		Assertions.assertEquals(Arrays.asList("libncursesw.so.6", "libc.so", "libdl.so"), ds.getNeededLibraries());
 
-		Assert.assertEquals("/system/bin/linker", file.getInterpreter());
+		Assertions.assertEquals("/system/bin/linker", file.getInterpreter());
 
 		// Dynamic section at offset 0x2e14 contains 26 entries:
 		// Tag Type Name/Value
@@ -88,70 +88,70 @@ public class BasicTest {
 		// 0x6ffffffb (FLAGS_1) Flags: NOW
 		// 0x00000000 (NULL) 0x0
 		ElfDynamicStructure dynamicStructure = file.getDynamicLinkSection().getDynamicSection();
-		Assert.assertEquals(26, dynamicStructure.entries.size());
-		Assert.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(3, 0xbf44), dynamicStructure.entries.get(0));
-		Assert.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(2, 352), dynamicStructure.entries.get(1));
-		Assert.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0x17, 0x8868), dynamicStructure.entries.get(2));
-		Assert.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0x6ffffffb, 1), dynamicStructure.entries.get(24));
-		Assert.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0, 0), dynamicStructure.entries.get(25));
+		Assertions.assertEquals(26, dynamicStructure.entries.size());
+		Assertions.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(3, 0xbf44), dynamicStructure.entries.get(0));
+		Assertions.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(2, 352), dynamicStructure.entries.get(1));
+		Assertions.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0x17, 0x8868), dynamicStructure.entries.get(2));
+		Assertions.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0x6ffffffb, 1), dynamicStructure.entries.get(24));
+		Assertions.assertEquals(new ElfDynamicStructure.ElfDynamicSectionEntry(0, 0), dynamicStructure.entries.get(25));
 	}
 
 	@Test
 	public void testAndroidArmLibNcurses() throws ElfException, FileNotFoundException, IOException {
 		ElfFile file = parseFile("android_arm_libncurses");
-		Assert.assertEquals(ElfFile.CLASS_32, file.objectSize);
-		Assert.assertEquals(ElfFile.DATA_LSB, file.encoding);
-		Assert.assertEquals(ElfFile.FT_DYN, file.file_type);
-		Assert.assertEquals(ElfFile.ARCH_ARM, file.arch);
-		Assert.assertEquals("/system/bin/linker", file.getInterpreter());
+		Assertions.assertEquals(ElfFile.CLASS_32, file.objectSize);
+		Assertions.assertEquals(ElfFile.DATA_LSB, file.encoding);
+		Assertions.assertEquals(ElfFile.FT_DYN, file.file_type);
+		Assertions.assertEquals(ElfFile.ARCH_ARM, file.arch);
+		Assertions.assertEquals("/system/bin/linker", file.getInterpreter());
 
 		List<ElfSection> noteSections = file.sectionsWithType(ElfSection.SHT_NOTE);
-		Assert.assertEquals(1, noteSections.size());
-		Assert.assertEquals(".note.gnu.gold-version", noteSections.get(0).getName());
-		Assert.assertEquals("GNU", noteSections.get(0).getNote().getName());
-		Assert.assertEquals(ElfNote.NT_GNU_GOLD_VERSION, noteSections.get(0).getNote().type);
-		Assert.assertEquals("gold 1.11", noteSections.get(0).getNote().descriptorAsBytes());
+		Assertions.assertEquals(1, noteSections.size());
+		Assertions.assertEquals(".note.gnu.gold-version", noteSections.get(0).getName());
+		Assertions.assertEquals("GNU", noteSections.get(0).getNote().getName());
+		Assertions.assertEquals(ElfNote.NT_GNU_GOLD_VERSION, noteSections.get(0).getNote().type);
+		Assertions.assertEquals("gold 1.11", noteSections.get(0).getNote().descriptorAsBytes());
 	}
 
 	@Test
 	public void testLinxAmd64BinDash() throws ElfException, FileNotFoundException, IOException {
 		ElfFile file = parseFile("linux_amd64_bindash");
-		Assert.assertEquals(ElfFile.CLASS_64, file.objectSize);
-		Assert.assertEquals(ElfFile.DATA_LSB, file.encoding);
-		Assert.assertEquals(ElfFile.FT_DYN, file.file_type);
-		Assert.assertEquals(ElfFile.ARCH_X86_64, file.arch);
-		Assert.assertEquals(56, file.ph_entry_size);
-		Assert.assertEquals(9, file.num_ph);
-		Assert.assertEquals(64, file.sh_entry_size);
-		Assert.assertEquals(64, file.ph_offset);
-		Assert.assertEquals(27, file.num_sh);
-		Assert.assertEquals(119544, file.sh_offset);
+		Assertions.assertEquals(ElfFile.CLASS_64, file.objectSize);
+		Assertions.assertEquals(ElfFile.DATA_LSB, file.encoding);
+		Assertions.assertEquals(ElfFile.FT_DYN, file.file_type);
+		Assertions.assertEquals(ElfFile.ARCH_X86_64, file.arch);
+		Assertions.assertEquals(56, file.ph_entry_size);
+		Assertions.assertEquals(9, file.num_ph);
+		Assertions.assertEquals(64, file.sh_entry_size);
+		Assertions.assertEquals(64, file.ph_offset);
+		Assertions.assertEquals(27, file.num_sh);
+		Assertions.assertEquals(119544, file.sh_offset);
 		assertSectionNames(file, null, ".interp", ".note.ABI-tag", ".note.gnu.build-id", ".gnu.hash", ".dynsym");
 
 		ElfDynamicStructure ds = file.getDynamicLinkSection().getDynamicSection();
-		Assert.assertEquals(Collections.singletonList("libc.so.6"), ds.getNeededLibraries());
+		Assertions.assertEquals(Collections.singletonList("libc.so.6"), ds.getNeededLibraries());
 
-		Assert.assertEquals("/lib64/ld-linux-x86-64.so.2", file.getInterpreter());
+		Assertions.assertEquals("/lib64/ld-linux-x86-64.so.2", file.getInterpreter());
 
 		ElfSection rodata = file.firstSectionByName(ElfSection.NAME_RODATA);
-		Assert.assertNotNull(rodata);
-		Assert.assertEquals(ElfSection.SHT_PROGBITS, rodata.type);
+		Assertions.assertNotNull(rodata);
+		Assertions.assertEquals(ElfSection.SHT_PROGBITS, rodata.type);
 
 		List<ElfSection> noteSections = file.sectionsWithType(ElfSection.SHT_NOTE);
-		Assert.assertEquals(2, noteSections.size());
-		Assert.assertEquals(".note.ABI-tag", noteSections.get(0).getName());
-		Assert.assertEquals("GNU", noteSections.get(0).getNote().getName());
-		Assert.assertEquals(ElfNote.NT_GNU_ABI_TAG, noteSections.get(0).getNote().type);
-		Assert.assertEquals(ElfNote.GnuAbiDescriptor.ELF_NOTE_OS_LINUX, noteSections.get(0).getNote().descriptorAsGnuAbi().operatingSystem);
-		Assert.assertEquals(2, noteSections.get(0).getNote().descriptorAsGnuAbi().majorVersion);
-		Assert.assertEquals(6, noteSections.get(0).getNote().descriptorAsGnuAbi().minorVersion);
-		Assert.assertEquals(24, noteSections.get(0).getNote().descriptorAsGnuAbi().subminorVersion);
-		Assert.assertEquals(".note.gnu.build-id", noteSections.get(1).getName());
-		Assert.assertEquals("GNU", noteSections.get(1).getNote().getName());
-		Assert.assertEquals(ElfNote.NT_GNU_BUILD_ID, noteSections.get(1).getNote().type);
-		Assert.assertEquals(0x14, noteSections.get(1).getNote().descriptorBytes().length);
-		Assert.assertEquals(0x0f, noteSections.get(1).getNote().descriptorBytes()[0]);
-		Assert.assertArrayEquals(new byte[]{0x0f, 0x7f, (byte) 0xf2, (byte) 0x87, (byte) 0xcf, 0x26, (byte) 0xeb, (byte) 0xa9, (byte) 0xa6, 0x64, 0x3b, 0x12, 0x26, 0x08, (byte) 0x9e, (byte) 0xea, 0x57, (byte) 0xcb, 0x7e, 0x44},
+		Assertions.assertEquals(2, noteSections.size());
+		Assertions.assertEquals(".note.ABI-tag", noteSections.get(0).getName());
+		Assertions.assertEquals("GNU", noteSections.get(0).getNote().getName());
+		Assertions.assertEquals(ElfNote.NT_GNU_ABI_TAG, noteSections.get(0).getNote().type);
+		Assertions.assertEquals(ElfNote.GnuAbiDescriptor.ELF_NOTE_OS_LINUX, noteSections.get(0).getNote().descriptorAsGnuAbi().operatingSystem);
+		Assertions.assertEquals(2, noteSections.get(0).getNote().descriptorAsGnuAbi().majorVersion);
+		Assertions.assertEquals(6, noteSections.get(0).getNote().descriptorAsGnuAbi().minorVersion);
+		Assertions.assertEquals(24, noteSections.get(0).getNote().descriptorAsGnuAbi().subminorVersion);
+		Assertions.assertEquals(".note.gnu.build-id", noteSections.get(1).getName());
+		Assertions.assertEquals("GNU", noteSections.get(1).getNote().getName());
+		Assertions.assertEquals(ElfNote.NT_GNU_BUILD_ID, noteSections.get(1).getNote().type);
+		Assertions.assertEquals(0x14, noteSections.get(1).getNote().descriptorBytes().length);
+		Assertions.assertEquals(0x0f, noteSections.get(1).getNote().descriptorBytes()[0]);
+		Assertions.assertArrayEquals(new byte[]{0x0f, 0x7f, (byte) 0xf2, (byte) 0x87, (byte) 0xcf, 0x26, (byte) 0xeb, (byte) 0xa9, (byte) 0xa6, 0x64, 0x3b, 0x12, 0x26, 0x08, (byte) 0x9e, (byte) 0xea, 0x57, (byte) 0xcb, 0x7e, 0x44},
 				noteSections.get(1).getNote().descriptorBytes());
 	}
 
