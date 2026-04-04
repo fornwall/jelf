54c2e77d36503fbe10ebc3c4a9b5d89c08f34f9e
Fredrik Fornwall <fredrik@fornwall.net>
2026-04-04 23:51:44 +0200

    Bump junit-jupiter from 5.14.2 to 5.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter:5.14.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.14.3")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 }
